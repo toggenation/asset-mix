049326c2b825d1f59d99d215cafa5b009921c81f
James McDonald <james@toggen.com.au>
2026-01-14 02:33:42 +0000

    rename Plugin to AssetMixPlugin

--- src/AssetMixPlugin.php
+++ src/AssetMixPlugin.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace AssetMix;
@@ -11,7 +12,7 @@ use Cake\Core\PluginApplicationInterface;
 /**
  * Plugin for AssetMix
  */
-class Plugin extends BasePlugin
+class AssetMixPlugin extends BasePlugin
 {
     /**
      * Add console commands for the plugin.
